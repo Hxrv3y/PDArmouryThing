791dfbfe8632e1423e0052b236e124eb139d0815
Harv <86970698+Hxrv3y@users.noreply.github.com>
2025-08-31 16:43:05 +0100

    git

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Harv'
+
+shared_scripts = {
+    'config.lua'
+}
+
+client_scripts {
+    'cl_armoury.lua'
+
+}
+
+server_scripts {
+    'sv_armoury.lua'
+
+}
